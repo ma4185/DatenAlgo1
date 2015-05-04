245ed9141834c38e0b99de53fb34c9fb712544a7
ma4185 <maren.osterloh@web.de>
2015-05-04 22:19:35 +0200

    Version 2.0

--- suchen.m
+++ suchen.m
@@ -1,85 +1,66 @@
-%function [ pe,sa, wo, ph] = suchen( Person, Satza, Wort, Phonem)
-function [ Ausgabe] = suchen( sucheNach, Was)
-% 
-% 1=Person
-% 2=Satz
-% 3=Wort
-% 4=Phonem
-% if suchNach == 1
-% ........
-
-
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [ AusgabeMatrix] = suchen3(sucheNachPHN, sucheNachWOR, sucheNachSAT, sucheNachPER)
 
 fid = fopen('allsenlist.txt');
 data = textscan(fid, '%s', 'delimiter', '\n');
 fclose(fid);
 
-% fid = fopen('allphonelist.txt');
-% data2 = textscan(fid, '%s', 'delimiter', '\n');
-% fclose(fid);
+fid = fopen('allphonelist.txt');
+data2 = textscan(fid, '%s', 'delimiter', '\n');
+fclose(fid);
 
-temp=size(data{1});
-zeilen=temp(1)+1;
+Eingabewerte1_3={sucheNachWOR sucheNachSAT sucheNachPER sucheNachPHN};
 
-if sucheNach==1
-counter=1;
-    while counter < zeilen
-%B=strfind(data{1}(counter), Person)
-        B=strfind(data{1}(counter), Was);
-        B_str=cell2mat(B);
-    
-        a=cell2mat(data{1}(counter));
-        Satz=mat2str(a);
-        if B_str > 1
-           Satz=strsplit(Satz);
-       %pe=Satz(1);
-           Ausgabe=Satz(1);
-        
-        end    
-    counter=counter+1;
+Zeilen=1:size(data{1});
+
+for Eingaben=1:4
+    if ~isempty(Eingabewerte1_3{Eingaben})
+        ZeilenTemp = [];
+        for idx_temp = Zeilen % 1:length(Zeilen)
+            if Eingaben < 4
+                B=strfind(data{1}(idx_temp), Eingabewerte1_3{Eingaben}); % Suche in der
+            else
+                B=strfind(data2{1}(idx_temp), Eingabewerte1_3{Eingaben}); % Suche in der
+            end
+            if ~isempty(cell2mat(B))
+                ZeilenTemp(end+1)=idx_temp;
+            end
+        end
+        Zeilen = ZeilenTemp; 
     end
+    
 end
-
-% while counter < zeilen
-%     B=strfind(data{1}(counter), Satza);
-%     B_str=cell2mat(B);
-%     
-%     a=cell2mat(data{1}(counter));
-%     Satz=mat2str(a);
+% counter=1;
+% data
+% AusgabeMatrix=cell2mat(data{1}(Zeilen)); % Alle Sätze als Matrix
+% Ausgabe=data{1}(Zeilen)
+ for i=1:length(Zeilen)
+     AusgabeMatrix(i)=data{1}(Zeilen(i));    
+ end     
+ 
+end
+% for idx_temp=1:length(Zeilen)
+%     B=strfind(data2{1}(Zeilen), sucheNachPHN); % Suche in der
+%     %                      einzelnen Zeile nach dem gewünschten Suchobjekt, B
+%     %                      =Zeichen, wo Übereinstimmung gefunden
+%     B_str=cell2mat(B); % Aus dem Cellarray wird eine Matrix
+%     %                      gemacht, damit später mit dem Wert gearbeitet
+%     %                      werden kann
 %     if B_str > 1
-%        Satz=strsplit(Satz);
-%        sa=Satz(1);
-%     end    
-%     counter=counter+1;
-% end
-
-% while counter < zeilen
-%     B=strfind(data{1}(counter), Wort);
-%     B_str=cell2mat(B);
-%     
-%     a=cell2mat(data{1}(counter));
-%     Satz=mat2str(a);
-%     if B_str > 1
-%        Satz=strsplit(Satz);
-%        wo=Satz(1);
-%     end    
-%     counter=counter+1;
+%         Zeilen(temp2)=idx_temp;
+%         temp2=temp2+1;
+%     end
 % end
 % 
-% while counter < zeilen
-%     B=strfind(data2{1}(counter), Phonem);
-%     B_str=cell2mat(B);
-%     
-%     a=cell2mat(data2{1}(counter));
-%     Satz=mat2str(a);
+% 
+% for i=1:length(Zeilen);
+%     Satz=mat2str(GefilterteSaetze);    % Matrix als String (für Stringsplit)
 %     if B_str > 1
-%        Satz=strsplit(Satz);
-%        ph=Satz(1);
-%     end    
-%     counter=counter+1;
+%         Satz=strsplit(Satz);
+%         AusgabePER(temp1)=Satz(1);
+%         temp1=temp1+1;
+%     else
+%         AusgabePER='NIX';
+%     end
 % end
-
-end
-
+% 
+% end
