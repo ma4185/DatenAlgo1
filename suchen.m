abe1f24096f28c1762ee0a3962740a80b0ed77fe
Maren Osterloh <maren.osterloh@web.de>
2015-05-04 09:25:40 +0200

    Version 04.05.2015

--- suchen.m
+++ suchen.m
@@ -1,4 +1,14 @@
-function [ pe,sa, wo, ph] = suchen( Person, Satza, Wort, Phonem)
+%function [ pe,sa, wo, ph] = suchen( Person, Satza, Wort, Phonem)
+function [ Ausgabe] = suchen( sucheNach, Was)
+% 
+% 1=Person
+% 2=Satz
+% 3=Wort
+% 4=Phonem
+% if suchNach == 1
+% ........
+
+
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,41 +16,44 @@ fid = fopen('allsenlist.txt');
 data = textscan(fid, '%s', 'delimiter', '\n');
 fclose(fid);
 
-fid = fopen('allphonelist.txt');
-data2 = textscan(fid, '%s', 'delimiter', '\n');
-fclose(fid);
+% fid = fopen('allphonelist.txt');
+% data2 = textscan(fid, '%s', 'delimiter', '\n');
+% fclose(fid);
 
 temp=size(data{1});
 zeilen=temp(1)+1;
 
+if sucheNach==1
 counter=1;
-while counter < zeilen
-    B=strfind(data{1}(counter), Person);
-    B_str=cell2mat(B);
+    while counter < zeilen
+%B=strfind(data{1}(counter), Person)
+        B=strfind(data{1}(counter), Was);
+        B_str=cell2mat(B);
     
-    a=cell2mat(data{1}(counter));
-    Satz=mat2str(a);
-    if B_str > 1
-       Satz=strsplit(Satz);
-       pe=Satz(1);
-       
-    end    
+        a=cell2mat(data{1}(counter));
+        Satz=mat2str(a);
+        if B_str > 1
+           Satz=strsplit(Satz);
+       %pe=Satz(1);
+           Ausgabe=Satz(1);
+        
+        end    
     counter=counter+1;
+    end
 end
 
-
-while counter < zeilen
-    B=strfind(data{1}(counter), Satza);
-    B_str=cell2mat(B);
-    
-    a=cell2mat(data{1}(counter));
-    Satz=mat2str(a);
-    if B_str > 1
-       Satz=strsplit(Satz);
-       sa=Satz(1);
-    end    
-    counter=counter+1;
-end
+% while counter < zeilen
+%     B=strfind(data{1}(counter), Satza);
+%     B_str=cell2mat(B);
+%     
+%     a=cell2mat(data{1}(counter));
+%     Satz=mat2str(a);
+%     if B_str > 1
+%        Satz=strsplit(Satz);
+%        sa=Satz(1);
+%     end    
+%     counter=counter+1;
+% end
 
 % while counter < zeilen
 %     B=strfind(data{1}(counter), Wort);
